0470144666bf755e258f7aeb4c45b3a6c0f56f3e
Ngurah Deva <wirandanaputra@gmail.com>
2024-05-03 12:39:58 +0700

    Initial Commit

---
+++ KisahLoka/KisahLokaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KisahLokaApp.swift
+//  KisahLoka
+//
+//  Created by MacBook Air on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct KisahLokaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
